c079f9fe0d9d5c1d6e954bfce2a8c392563d2f88
csbao <calvin.bao94@gmail.com>
2016-11-04 18:55:36 -0700

    Fix sCreator and qsub args

--- sCreator.py
+++ sCreator.py
@@ -1,46 +1,35 @@
 import sys
 import subprocess
-import os
 
 
-def makeQsub(outDir, iterations=10,restarts=2):
-    for fold in range(10):
-        for it in range(restarts):
-            queue = "small.q"
-            jobName = "h_%s_F%d_%d" % (outDir, fold, it)
 
-            echoArgs = ["/campusdata/csbao/usr/local/bin/python3.3", "/campusdata/csbao/sluicing/scripts/xml_parser.py", "-iterations", str(iterations)]#, "/campusdata/csbao/test3/" + features, str(fold), foldFile, "-ignoreFeats", ignoreSet]
 
-            outputFile = "/campusdata/csbao/sluicing/scripts/%s/i_F%d_%d" % (outDir, fold,it)
-            errorFile = "/campusdata/csbao/sluicing/scripts/%s/e_F%d_%d" % (outDir, fold,it)
+def makeQsub(dividend=7, count=4634):
 
-            qsubArgs = ["qsub", "-cwd", "-b", "y", "-V", "-q", queue, "-N", jobName, "-o", outputFile, "-e", errorFile]
-            wholeCmd = " ".join(qsubArgs) + " " + " ".join(echoArgs)
-            print (wholeCmd)
-            out = subprocess.Popen(wholeCmd, shell=True, stdout=subprocess.PIPE)
-            out = out.communicate()[0]
-            jobId = out.split()[2]
+    prev = 0
+    for i in range(1,dividend+1):
+        curr = count/dividend * i
 
-def makeQsub2():
-   
-    job_param1 = 12.5
-    job_param2 = 5.0
-    jobName = "python my_script.py %f %f" % (job_param1,job_param2)
-    cmd = "module load Python/2.7; sleep 0.2; %s" % jobName
-    echoArgs = ["echo", "-e", "'%s'" % cmd]
-    print(" ".join(echoArgs))
-    qsubArgs = ["qsub","-cwd"]
-    print(" ".join(qsubArgs))
+        queue = "small.q"
+        echoArgs = ["/campusdata/csbao/usr/local/bin/python3.3", "/campusdata/csbao/sluicing/scripts/xml_parser.py", str(prev), str(curr)]
+        outputFile = "/campusdata/csbao/sluicing/scripts/%s/output_%d" % (i)
+        errorFile = "/campusdata/csbao/sluicing/scripts/%s/error_%d" % (i)
 
-    wholeCmd = " ".join(echoArgs) + " | " + " ".join(qsubArgs)
-    out = subprocess.Popen(wholeCmd, shell=True, stdout=subprocess.PIPE)
-    out = out.communicate()[0]
+        qsubArgs = ["qsub", "-cwd", "-b", "y", "-V", "-q", queue, "-o", outputFile, "-e", errorFile]
+        wholeCmd = " ".join(qsubArgs) + " ".join(echoArgs)
+        out = subprocess.Popen(wholeCmd, shell=True, stdout=subprocess.PIPE)
+        out = out.communicate()[0]
+        jobId = out.split()[2]
+        print (jobId)
+        prev = curr
 
-    jobId = out.split()[2]
-    print jobId
+
+
 
 def main():
-    makeQsub("work", iterations=50, restarts=50)
+    makeQsub()
+    # initialize dividend to 7 and count to 4634 since it splits evenly.
+    # enter length of rows into the function for now.
 
 if __name__=="__main__":
     main()
